cb2415f7a2ac2a04c19733b0951180c7de15d171
pasepulvedar <paula.sepulveda.94@gmail.com>
2023-12-20 09:59:39 -0300

    quitar espacios blancos

--- demo_2_analytics/st_demo_2_analytics.py
+++ demo_2_analytics/st_demo_2_analytics.py
@@ -17,7 +17,6 @@ st.write(
     unsafe_allow_html=True,
 )
 
-#####
 st.markdown("""
         <style>
                .block-container {
@@ -28,7 +27,6 @@ st.markdown("""
                 }
         </style>
         """, unsafe_allow_html=True)
-######
 
 hide_decoration_bar_style = '<style> header {visibility: hidden;} </style>'
 st.markdown(hide_decoration_bar_style, unsafe_allow_html=True)
--- demo_3_simulator/st_demo_3_simulator.py
+++ demo_3_simulator/st_demo_3_simulator.py
@@ -22,6 +22,17 @@ st.write(
     unsafe_allow_html=True,
 )
 
+st.markdown("""
+        <style>
+               .block-container {
+                    padding-top: 1rem;
+                    padding-bottom: 0rem;
+                    padding-left: 5rem;
+                    padding-right: 5rem;
+                }
+        </style>
+        """, unsafe_allow_html=True)
+        
 hide_decoration_bar_style = '<style> header {visibility: hidden;} </style>'
 st.markdown(hide_decoration_bar_style, unsafe_allow_html=True)
 
