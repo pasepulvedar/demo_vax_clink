89f3d928f28f92aaa33ca89cb7d6948b277cd746
pasepulvedar <paula.sepulveda.94@gmail.com>
2023-12-20 10:06:59 -0300

    correccion ortografia

--- demo_3_simulator/st_demo_3_simulator.py
+++ demo_3_simulator/st_demo_3_simulator.py
@@ -32,13 +32,13 @@ st.markdown("""
                 }
         </style>
         """, unsafe_allow_html=True)
-        
+
 hide_decoration_bar_style = '<style> header {visibility: hidden;} </style>'
 st.markdown(hide_decoration_bar_style, unsafe_allow_html=True)
 
 fig_palette =  ['#00857C', '#6ECEB2', '#0C2340', '#BFED33', '#FFF063', '#69B8F7', '#688CE8', '#5450E4']
 
-st.header('Finantial Simualtor & Adherence Tools')
+st.header('Finantial Simulator & Adherence Tools')
 
 tab1, tab2 = st.tabs(['Finantial Simulator', "Adherence Tools"])
 
